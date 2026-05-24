c8d37d7eb1a42b7166f521ab360a4d35f7645a2b
yusufbtanriverdi <45808617+yusufbtanriverdi@users.noreply.github.com>
2026-05-24 06:50:39 +0000

    Deploying to gh-pages from @ yusufbtanriverdi/yusufbtanriverdi.github.io@ff3bcd37b9945ded3b6b4ac5f14515116dd50aa5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,14 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%79%75%73%75%66%62%61%72%61%6E.%74%61%6E%72%69%76%65%72%64%69@%75%70%66.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -539,14 +532,14 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=kZtYpaUAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.upf.edu/web/bcn-medtech", "_blank");
         },
       },{
       id: 'light-theme',
