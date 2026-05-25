4db13b11457fcb2af7581b10e4d5ce668b56bed7
yusufbtanriverdi <45808617+yusufbtanriverdi@users.noreply.github.com>
2026-05-25 08:22:51 +0000

    Deploying to gh-pages from @ yusufbtanriverdi/yusufbtanriverdi.github.io@edae1b5b1a69c8a2bdf51c317c5786336f760be7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Curriculum vitea.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -482,16 +482,16 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "teachings-data-science-fundamentals",
-          title: 'Data Science Fundamentals',
-          description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
+            },},{id: "teachings-deep-learning",
+          title: 'Deep Learning',
+          description: "",
           section: "Teachings",handler: () => {
-              window.location.href = "/teachings/data-science-fundamentals/";
-            },},{id: "teachings-introduction-to-machine-learning",
-          title: 'Introduction to Machine Learning',
-          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
+              window.location.href = "/teachings/deep_learning/";
+            },},{id: "teachings-video-processing",
+          title: 'Video Processing',
+          description: "",
           section: "Teachings",handler: () => {
-              window.location.href = "/teachings/introduction-to-machine-learning/";
+              window.location.href = "/teachings/video_processing/";
             },},{
         id: 'social-cv',
         title: 'CV',
